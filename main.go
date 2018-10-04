847fe5a4f72f0c90cbdc4907d60787d261778c39
EeOneDown <rentgen.stalker@yandex.ru>
2018-10-04 19:23:16 +0300

    closes #1

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
+	"time"
 )
 
+func dateHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+
+	json.NewEncoder(w).Encode(time.Now().Local())
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-type", "text/plain")
-		w.Write([]byte("Hello World!!!"))
-	})
+	http.HandleFunc("/", dateHandler)
+
 	http.ListenAndServe(":8080", nil)
 }
