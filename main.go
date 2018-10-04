e7dbe721eb88ef745d5c364e79c3c2338df1d13f
EeOneDown <rentgen.stalker@yandex.ru>
2018-10-04 19:29:13 +0300

    closes #2

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 	"time"
 )
 
@@ -12,8 +13,24 @@ func dateHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(time.Now().Local())
 }
 
+func waitHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+
+	res := "Nothing to wait"
+
+	strVal := r.URL.Query().Get("delay")
+	if _, err := strconv.Atoi(strVal); err == nil {
+		dur, _ := time.ParseDuration(strVal + "ms")
+		time.Sleep(dur)
+		res = "Sleep for " + dur.String() + " completed"
+	}
+
+	json.NewEncoder(w).Encode(res)
+}
+
 func main() {
 	http.HandleFunc("/date", dateHandler)
+	http.HandleFunc("/wait", waitHandler)
 
 	http.ListenAndServe(":8080", nil)
 }
